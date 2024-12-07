3a410467c0948435a110d55d16e6c47cc80bcfe0
Darkrentin <79848980+Darkrentin@users.noreply.github.com>
2024-12-07 22:47:40 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,12 @@
 @echo off
-echo test du script
+setlocal enabledelayedexpansion
+
+echo Recherche du fichier "Star Citizen" dans le dossier actuel...
+
+:: Recherche dans le répertoire actuel et ses sous-dossiers
+for /r %%f in (*Star Citizen*) do (
+    echo %%f
+)
+
+echo Recherche terminée.
 pause
