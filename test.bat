ef4b04fc08158a7d67db698da359aedd558aaeec
Darkrentin <79848980+Darkrentin@users.noreply.github.com>
2024-12-07 23:01:10 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 echo Recherche du fichier "StarCitizen" dans le dossier actuel...
 
 :: Recherche dans le dossier actuel et ses sous-répertoires
-for /d /r %%f in (StarCitizen) do (
-    echo Fichier trouvé : %%f
+for /d /r %%d in (StarCitizen) do (
+    echo Dossier trouvé : %%d
     goto :found
 )
 
